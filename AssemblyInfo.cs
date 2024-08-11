6cacaea54ff1eac51be2d7310d4dff882437721a
Josiah823 <jhfsdaga76taysdu@outlook.com>
2024-08-11 18:26:46 +0400

    AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyTitle("Reverse Proxy")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("FireSocks")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("A-B Security")]
+[assembly: AssemblyFileVersion("1.3.1.2")]
+[assembly: Guid("ecc95594-6730-435b-901f-7becb795a62a")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyVersion("1.3.1.2")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
